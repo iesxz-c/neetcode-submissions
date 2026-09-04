f1b1c6f8f6a58c33f9019b5f04c551ae5dff88c8
Akash <akash.krishnan.046@gmail.com>
2026-09-04 12:25:45 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tc=Counter(t)
+        c=len(tc)
+        ans = ""
+        l=0
+        formed=0
+        have={}
+        for r in range(len(s)):
+            have[s[r]] = have.get(s[r],0)+1
+            if have[s[r]] == tc[s[r]]:
+                formed+=1
+            while formed == c:
+                if not ans or len(ans) > len( s[l:r+1]):
+                    ans = "".join(s[l:r+1])
+                have[s[l]] -=1
+                if s[l] in tc:
+                    formed-=1
+                l+=1
+        return ans
+
+            
